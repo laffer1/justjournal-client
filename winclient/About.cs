a39756fdd6a2838907e24188afcc10a4af9d115c
laffer1 <laffer1>
2005-11-24 17:40:10 +0000

    no message

---
+++ winclient/About.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace JustJournal
+{
+	/// <summary>
+	/// About Just Journal
+	/// </summary>
+	public class AboutForm : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label lblVersion;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public AboutForm()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+            lblVersion.Text = JustJournal.Version;
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(AboutForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.lblVersion = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(400, 72);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// lblVersion
+			// 
+			this.lblVersion.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.lblVersion.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.lblVersion.Location = new System.Drawing.Point(104, 160);
+			this.lblVersion.Name = "lblVersion";
+			this.lblVersion.Size = new System.Drawing.Size(208, 16);
+			this.lblVersion.TabIndex = 8;
+			this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(72, 80);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(312, 48);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "This software allows you to post journal entries using the just journal website. " +
+				" Copyright 2005 Lucas Holt";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+			this.linkLabel1.Location = new System.Drawing.Point(16, 128);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(368, 23);
+			this.linkLabel1.TabIndex = 10;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "http://www.justjournal.com";
+			this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(96, 184);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(216, 152);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 11;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(184, 344);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(48, 23);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "Tigress";
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(24, 80);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(40, 32);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 13;
+			this.pictureBox3.TabStop = false;
+			// 
+			// AboutForm
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(402, 368);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblVersion);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AboutForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "About JustJournal";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		private void linkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+		{
+			System.Diagnostics.Process.Start("http://www.justjournal.com/");
+		}
+
+		
+	}
+}
