72b902ee3f81a59dc1a8bd3d1b6d3e2a2471e72a
laffer1 <laffer1>
2008-06-19 23:39:48 +0000

    Add copy and paste feature.

--- unixclient/gtk/gtkjj.c
+++ unixclient/gtk/gtkjj.c
@@ -1,4 +1,4 @@
-/* $Id: gtkjj.c,v 1.2 2008/06/18 03:23:23 laffer1 Exp $ */
+/* $Id: gtkjj.c,v 1.3 2008/06/19 23:39:48 laffer1 Exp $ */
 /*-
 Copyright (C) 2008 Lucas Holt. All rights reserved.
 
@@ -42,23 +42,46 @@ GtkTextBuffer *buffer;
 
 static void button_clicked (GtkButton *button, GtkWindow *parent);
 static void msgbox( GtkWindow *parent, char * msg );
+static void cut_clicked (GtkButton*, GtkTextView*);
+static void copy_clicked (GtkButton*, GtkTextView*);
+static void paste_clicked (GtkButton*, GtkTextView*);
 
 int main( int argc, char *argv[] )
 {
-    GtkWidget *window, *vbox, *authbox, *authbox2, *vauthbox;
+    GtkWidget *window, *vbox, *authbox, *authbox2, *vauthbox, *hboxccp;
     GtkWidget *lbluser, *lblpass; /* labels */
-    GtkWidget *submit; /* buttons */
+    GtkWidget *submit, *cut, *copy, *paste; /* buttons */
     GtkWidget *scrolled_win, *textview;
 
     gtk_init( &argc, &argv );
 
     window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
-    gtk_window_set_title( GTK_WINDOW (window), "Just Journal" );
+    gtk_window_set_title( GTK_WINDOW (window), NAME );
     gtk_container_set_border_width( GTK_CONTAINER (window), 10 );
     //gtk_widget_set_size_request( window, 200, -1 );
 
     g_signal_connect (G_OBJECT (window), "destroy",
                   G_CALLBACK (gtk_main_quit), NULL);
+
+    cut = gtk_button_new_from_stock (GTK_STOCK_CUT);
+    copy = gtk_button_new_from_stock (GTK_STOCK_COPY);
+    paste = gtk_button_new_from_stock (GTK_STOCK_PASTE);
+
+    hboxccp = gtk_hbox_new (TRUE, 5);
+    gtk_box_pack_start (GTK_BOX (hboxccp), cut, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (hboxccp), copy, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (hboxccp), paste, TRUE, TRUE, 0);
+
+
+    g_signal_connect (G_OBJECT (cut), "clicked",
+                    G_CALLBACK (cut_clicked),
+                    (gpointer) textview);
+    g_signal_connect (G_OBJECT (copy), "clicked",
+                    G_CALLBACK (copy_clicked),
+                    (gpointer) textview);
+    g_signal_connect (G_OBJECT (paste), "clicked",
+                    G_CALLBACK (paste_clicked),
+                    (gpointer) textview);
 
     submit = gtk_button_new_with_mnemonic("_Post");
     g_signal_connect (G_OBJECT (submit), "clicked",
@@ -73,7 +96,7 @@ int main( int argc, char *argv[] )
     gtk_entry_set_invisible_char( GTK_ENTRY (pass), '*' );
 
     /* setup textview */
-    textview = gtk_text_view_new ();
+    textview = gtk_text_view_new();
     buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (textview));
     scrolled_win = gtk_scrolled_window_new (NULL, NULL);
     gtk_container_add (GTK_CONTAINER (scrolled_win), textview);
@@ -96,6 +119,7 @@ int main( int argc, char *argv[] )
 
     /* Setup the final box for layout in the window */
     vbox = gtk_vbox_new( FALSE, 5 );
+    gtk_box_pack_start( GTK_BOX (vbox), hboxccp, FALSE, TRUE, 5 );
     gtk_box_pack_start( GTK_BOX (vbox), vauthbox, FALSE, TRUE, 5 );
     gtk_box_pack_start( GTK_BOX (vbox), scrolled_win, TRUE, TRUE, 5 );
     gtk_box_pack_start( GTK_BOX (vbox), submit, FALSE, TRUE, 5 );
@@ -158,6 +182,11 @@ static void button_clicked( GtkButton *button, GtkWindow *parent )
         msgbox( parent, errmsg);
         free(errmsg);
     }
+    /* if we get here it worked.  Clear the entry data */
+    gtk_entry_set_text( GTK_ENTRY (user), "" );
+    gtk_entry_set_text( GTK_ENTRY (pass), "" );
+    gtk_text_buffer_set_text (buffer, "", -1);
+
 cleanup:
     g_free(c);
     xmlrpc_DECREF( resultP );
@@ -192,4 +221,36 @@ static void msgbox( GtkWindow * parent, char * msg )
     /* Call gtk_widget_destroy() when the dialog emits the response signal. */
     g_signal_connect (G_OBJECT (dialog), "response",
                     G_CALLBACK (gtk_widget_destroy), NULL);
+}
+
+/* Copy the selected text to the clipboard and remove it from the buffer. */
+static void
+cut_clicked (GtkButton *cut,
+             GtkTextView *textview)
+{
+    GtkClipboard *clipboard = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
+    //GtkTextBuffer *buffer2 = gtk_text_view_get_buffer (textview);
+
+    gtk_text_buffer_cut_clipboard (buffer, clipboard, TRUE);
+}
+
+/* Copy the selected text to the clipboard. */
+static void
+copy_clicked (GtkButton *copy,
+              GtkTextView *textview)
+{
+    GtkClipboard *clipboard = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
+
+    gtk_text_buffer_copy_clipboard (buffer, clipboard);
+}
+
+/* Insert the text from the clipboard into the text buffer. */
+static void
+paste_clicked (GtkButton *paste,
+               GtkTextView *textview)
+{
+    GtkClipboard *clipboard = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
+    //GtkTextBuffer *buffer2 = gtk_text_view_get_buffer (textview);
+
+    gtk_text_buffer_paste_clipboard (buffer, clipboard, NULL, TRUE);
 }
