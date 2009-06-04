5a391284d02e11dd346cfd4258b1776df315e515
laffer1 <laffer1>
2009-06-04 05:10:33 +0000

    Add new -r flag to get recent blog entries.

--- unixclient/jj.c
+++ unixclient/jj.c
@@ -1,5 +1,5 @@
 /*-
-Copyright (C) 2008 Lucas Holt. All rights reserved.
+Copyright (C) 2008, 2009 Lucas Holt. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -22,7 +22,7 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGE.
 
-$Id: jj.c,v 1.8 2008/09/27 01:15:48 laffer1 Exp $
+$Id: jj.c,v 1.9 2009/06/04 05:10:33 laffer1 Exp $
 */
 
 #include <stdio.h>
@@ -44,9 +44,11 @@ $Id: jj.c,v 1.8 2008/09/27 01:15:48 laffer1 Exp $
 #define PASSLEN 19
 #define HOSTLEN 512
 #define HOST "www.justjournal.com"
+#define RECENT_POST_COUNT 15
 
 static void usage( const char *name );
 static void die_if_fault_occurred( xmlrpc_env *env );
+static void getRecentPosts(const char *host, const char *username, const char *password);
 
 int main( int argc, char *argv[] )
 {
@@ -65,11 +67,12 @@ int main( int argc, char *argv[] )
     bool hflag = false;
     bool uflag = false;
     bool pflag = false;
+    bool rflag = false;
 
     if ( argc < 3 )
         usage( argv[0] );
     
-    while ((c = getopt( argc, argv, "h:u:p:s:d" )) != -1) {
+    while ((c = getopt( argc, argv, "h:u:p:s:dr" )) != -1) {
         switch( c )
         {
             case 'h': /* host */	
@@ -84,17 +87,17 @@ int main( int argc, char *argv[] )
          
             case 'u': /* username */
                 strncpy( username, optarg, USERLEN - 1 );
-		        username[USERLEN -1] = '\0';
-		        uflag = true;
+                username[USERLEN -1] = '\0';
+                uflag = true;
                 break;
          
-            case 'p':
+            case 'p': /* password */
                 strncpy( password, optarg, PASSLEN - 1 );
                 password[PASSLEN -1] = '\0';
                 pflag = true;
                 break;
                 
-            case 's':
+            case 's': /* subject */
             	titlelen = strlen(optarg);
             	if ( (title = malloc((titlelen * sizeof(char)) + 1)) == NULL )
             	{
@@ -105,7 +108,11 @@ int main( int argc, char *argv[] )
                 title[titlelen] = '\0';
                 break;
                 
-            case 'd':
+            case 'r': /* recent entries */
+                rflag = true;
+                break;
+                
+            case 'd': /* debug */
                 debug = true;
                 fprintf( stderr, "Debug enabled.\n");
                 break;
@@ -129,6 +136,12 @@ int main( int argc, char *argv[] )
         
     if ( debug && hflag )
         fprintf( stderr, "host is set to: %s\n", host );
+
+    if ( rflag )
+    {
+        getRecentPosts( host, username, password );
+        exit(0);
+    }
 
     /* Copy the title into the top of entry if it's requested */
     entry[0] = '\0';
@@ -157,12 +170,12 @@ int main( int argc, char *argv[] )
     resultP = xmlrpc_client_call( &env, host,
                                 "blogger.newPost",
                                 "(sssssb)", 
-				"", /* key, not used */
+                                "", /* key, not used */
                                 username, /* journal unique name */
-				username, /* journal username */
-				password, /* journal password */
-				entry, /* blog content */
-				true ); /* post now */
+                                username, /* journal username */
+                                password, /* journal password */
+                                entry, /* blog content */
+                                true ); /* post now */
     die_if_fault_occurred( &env );
 
     xmlrpc_read_string( &env, resultP, &postResult );
@@ -187,11 +200,11 @@ static void usage( const char *name )
 static void die_if_fault_occurred( xmlrpc_env *env )
 {
     if ( env == NULL )
-	exit(EXIT_FAILURE);
+	    exit(EXIT_FAILURE);
 
     if ( env->fault_occurred )
     {
-        if (env->fault_code == -501)
+        if ( env->fault_code == -501 )
             fprintf( stderr, "ERROR:  Your account info %s",
                              "might be incorrect.\n" );
         else
@@ -201,3 +214,58 @@ static void die_if_fault_occurred( xmlrpc_env *env )
     }
 }
 
+static void getRecentPosts(const char *host, const char *username, const char *password)
+{
+    xmlrpc_env env;
+    xmlrpc_value * resultP; // array item 
+    xmlrpc_value * firstElementP;  // first element in array
+    xmlrpc_value * varP;
+    const char * postResult;
+    int arrsize;
+    
+    xmlrpc_client_init( XMLRPC_CLIENT_NO_FLAGS, NAME, VERSION );
+    xmlrpc_env_init( &env );
+
+    resultP = xmlrpc_client_call( &env, host,
+                                "blogger.getRecentPosts",
+                                "(ssssi)", 
+                                "", /* key, not used */
+                                username, /* journal unique name */
+                                username, /* journal username */
+                                password, /* journal password */
+                                RECENT_POST_COUNT ); /* post count */
+    die_if_fault_occurred( &env );
+ 
+    arrsize = xmlrpc_array_size( &env, resultP ); 
+    die_if_fault_occurred( &env );
+    //fprintf( stderr, "Array size %d\n", arrsize );
+    
+    for (int i = 0; i < arrsize; i++ )
+    {
+        xmlrpc_array_read_item( &env, resultP, i, &firstElementP);
+        xmlrpc_struct_find_value( &env, firstElementP, "title", &varP);
+        if (varP)
+        {
+            xmlrpc_read_string( &env, varP, &postResult);
+            printf( "%d %s\n\n", i, postResult );
+            free((char *)postResult);
+            xmlrpc_DECREF( varP );
+        }
+        die_if_fault_occurred( &env );
+        
+        xmlrpc_struct_find_value( &env, firstElementP, "content", &varP);
+        if (varP)
+        {
+            xmlrpc_read_string( &env, varP, &postResult);
+            printf( "%s\n\n", postResult );
+            free((char *)postResult);
+            xmlrpc_DECREF( varP );
+        }
+        die_if_fault_occurred( &env );
+
+        xmlrpc_DECREF( firstElementP );
+    }
+    xmlrpc_DECREF( resultP );
+    xmlrpc_env_clean( &env );
+    xmlrpc_client_cleanup();
+}
