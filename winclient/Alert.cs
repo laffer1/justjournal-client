eb166fbba1d04ac08e6387dc631a4cb66e7f540f
Lucas Holt <luke@foolishgames.com>
2013-06-07 18:39:27 -0400

    clean

--- winclient/Alert.cs
+++ winclient/Alert.cs
@@ -1,42 +1,26 @@
 using System;
-using System.Drawing;
 using System.ComponentModel;
-using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace JustJournal
 {
-    partial class Alert 
+    partial class Alert
     {
-        [DllImport(dllName: "User32.dll")]
-        public extern static int ShowWindow(IntPtr hWnd, int cmdShow);
-
 // ReSharper disable InconsistentNaming
-        const Int32 SW_SHOWNOACTIVATE = 4;
+        private const Int32 SW_SHOWNOACTIVATE = 4;
 // ReSharper restore InconsistentNaming
 
-        [DllImport(dllName: "User32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public extern static bool SetWindowPos(
-            IntPtr hWnd,             // handle to window
-            Int32 hWndInsertAfter,  // placement-order handle
-            Int32 X,                 // horizontal position
-            Int32 Y,                 // vertical position
-            Int32 cx,                // width
-            Int32 cy,                // height
-            Int32 uFlags            // window-positioning options
-            );
-
         // ReSharper disable InconsistentNaming
-        const Int32 SWP_NOACTIVATE = 0x0010;
-        const Int32 HWND_TOPMOST = -1;
+        private const Int32 SWP_NOACTIVATE = 0x0010;
+        private const Int32 HWND_TOPMOST = -1;
         // ReSharper restore InconsistentNaming
 
-        [DllImport(dllName: "winmm.dll", CharSet = CharSet.Unicode)]
-        private static extern Int32 PlaySound(String lpszName, Int32 hModule, Int32 dwFlags);
         private static int SND_ALIAS = 0x00010000;
 
-        private string _linked;
+        private readonly string _linked;
 
         public Alert(string Message, string link)
         {
@@ -47,11 +31,29 @@ namespace JustJournal
             int w = 168;
             int cx = Screen.PrimaryScreen.WorkingArea.Width - (w + 25);
             int cy = Screen.PrimaryScreen.WorkingArea.Height - h;
-            this.SetBounds(cx, cy, w, h, BoundsSpecified.All);
-            ShowWindow(this.Handle, SW_SHOWNOACTIVATE);
-            SetWindowPos(this.Handle, HWND_TOPMOST, cx, cy, this.Width, this.Height, SWP_NOACTIVATE);
+            SetBounds(cx, cy, w, h, BoundsSpecified.All);
+            ShowWindow(Handle, SW_SHOWNOACTIVATE);
+            SetWindowPos(Handle, HWND_TOPMOST, cx, cy, Width, Height, SWP_NOACTIVATE);
             timer1.Start();
         }
+
+        [DllImport(dllName: "User32.dll")]
+        public static extern int ShowWindow(IntPtr hWnd, int cmdShow);
+
+        [DllImport(dllName: "User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(
+            IntPtr hWnd, // handle to window
+            Int32 hWndInsertAfter, // placement-order handle
+            Int32 X, // horizontal position
+            Int32 Y, // vertical position
+            Int32 cx, // width
+            Int32 cy, // height
+            Int32 uFlags // window-positioning options
+            );
+
+        [DllImport(dllName: "winmm.dll", CharSet = CharSet.Unicode)]
+        private static extern Int32 PlaySound(String lpszName, Int32 hModule, Int32 dwFlags);
 
         private void Alert_Load(object sender, EventArgs e)
         {
@@ -69,10 +71,10 @@ namespace JustJournal
             }
         }
 
-        private void msg_MouseLeave(object sender, System.EventArgs e)
+        private void msg_MouseLeave(object sender, EventArgs e)
         {
             Font ft = msg.Font;
-            msg.ForeColor = this.ForeColor;
+            msg.ForeColor = ForeColor;
             msg.Font = new Font(ft.FontFamily, ft.Size, FontStyle.Regular);
             msg.Cursor = Cursors.Default;
         }
@@ -83,7 +85,7 @@ namespace JustJournal
             {
                 if (!String.IsNullOrEmpty(_linked))
                 {
-                    System.Diagnostics.Process.Start(_linked);
+                    Process.Start(_linked);
                     timer1.Stop();
                     Close();
                 }
@@ -104,6 +106,5 @@ namespace JustJournal
         {
             Close();
         }
-
-   }
+    }
 }
